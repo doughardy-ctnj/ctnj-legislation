b0d506d170d1b693e4c60ba611332f19da43d6e0
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-03-04 17:08:22 -0500

    Allow vote switching
    This commit allows users to switch their votes on bills they have already voted for.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -75,23 +75,20 @@ class BillsController < ApplicationController
 
   def vote
     authorize Bill
-    if Vote.find_by_user_id_and_bill_id(current_user.id, params[:bill_id])
-      respond_to do |format|
-        format.html { redirect_back fallback_location: root_path, alert: 'Sorry, you already shared your opinion on this bill.' }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
+    if @vote = Vote.find_by_user_id_and_bill_id(current_user.id, params[:bill_id])
+      @vote.update(points: params[:points])
     else
       @vote = Vote.new(user_id: current_user.id,
                        bill_id: params[:bill_id],
                        points: params[:points])
-      respond_to do |format|
-        if @vote.save
-          format.html { redirect_to @vote.bill, flash: { success: 'Your vote has been recorded.' } }
-          format.json { render :show, status: :created, location: @vote }
-        else
-          format.html { redirect_back fallback_location: root_path, warning: 'Sorry, there was an error.' }
-          format.json { render json: @vote.errors, status: :unprocessable_entity }
-        end
+    end
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_to @vote.bill, flash: { success: 'Your vote has been recorded.' } }
+        format.json { render :show, status: :created, location: @vote }
+      else
+        format.html { redirect_back fallback_location: root_path, warning: 'Sorry, there was an error.' }
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
     end
   end
