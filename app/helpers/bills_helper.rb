349253dfc55b8d7fa31496cadadcca679b1989e2
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-03-18 20:46:34 -0400

    Revert "Resolve #27 Related Articles"
    This reverts commit e974d65771caaf8ae9854540b4614ce8d1df0310.

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -7,10 +7,6 @@ module BillsHelper
     end
   end
 
-  def related_articles_url(bill)
-    'http://www.ctnewsjunkie.com/archives/taglist/' + bill.data['session'] + bill.data['bill_id'].gsub(/\s/,'-')
-  end
-
   def thumbs_up_vote_count(bill)
     positive_votes = bill.votes.pluck(:points).select { |a| a.positive? }
     positive_votes.count
